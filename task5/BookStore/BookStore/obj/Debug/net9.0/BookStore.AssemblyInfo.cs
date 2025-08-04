1736f6818304f03be452546976dfa45fb40862e4
Kirill <khorosko15@gmail.com>
2025-08-04 03:05:39 +0300

    Update task5

--- task5/BookStore/BookStore/obj/Debug/net9.0/BookStore.AssemblyInfo.cs
+++ task5/BookStore/BookStore/obj/Debug/net9.0/BookStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+950a5519f6e5ad71afd6585cb23fbbf54992984f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e35a6a152acd6cda76ac7e1468f093ea5b0bce9")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
