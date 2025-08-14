84f4eddc0257ff4c8168a5be4e4391dd1d81e866
Kirill <khorosko15@gmail.com>
2025-08-14 22:29:48 +0300

    Add admin

--- CourseWork/CourseWork/CourseWork/Program.cs
+++ CourseWork/CourseWork/CourseWork/Program.cs
@@ -13,7 +13,10 @@ builder.Services.AddDbContext<CourseWorkDbContext>(options =>
 
 builder.Services.AddIdentity<User, IdentityRole>(options =>
 {
-    options.SignIn.RequireConfirmedAccount = true; 
+    options.SignIn.RequireConfirmedAccount = true;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequiredLength = 0;
 })
     .AddEntityFrameworkStores<CourseWorkDbContext>()
     .AddDefaultTokenProviders();
@@ -21,9 +24,67 @@ builder.Services.AddIdentity<User, IdentityRole>(options =>
 var app = builder.Build();
 
 app.UseRouting();         
-
 app.UseAuthentication();   
 app.UseAuthorization();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    try
+    {
+        var dbContext = services.GetRequiredService<CourseWorkDbContext>();
+        dbContext.Database.Migrate();
+
+        var userManager = services.GetRequiredService<UserManager<User>>();
+        var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+
+        string[] roleNames = { "Admin", "User" };
+        foreach (var roleName in roleNames)
+        {
+            if (!await roleManager.RoleExistsAsync(roleName))
+            {
+                await roleManager.CreateAsync(new IdentityRole(roleName));
+            }
+        }
+
+
+        var adminUser = await userManager.FindByEmailAsync("admin@admin.com");
+
+        if (adminUser == null)
+        {
+            adminUser = new User
+            {
+                UserName = "admin",
+                Email = "admin@admin.com",
+                FirstName = "admin",
+                LastName = "admin",
+                EmailConfirmed = true
+            };
+
+            string adminPassword = "admin123";
+
+            var result = await userManager.CreateAsync(adminUser, adminPassword);
+
+            if (result.Succeeded)
+            {
+                await userManager.AddToRoleAsync(adminUser, "Admin");
+            }
+            else
+            {
+                foreach (var error in result.Errors)
+                {
+                    Console.WriteLine($"Error: {error.Description}");
+                }
+                throw new Exception("Admin was not created");
+            }
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error: {ex.Message}");
+    }
+}
 
 
 app.MapGet("/", () => "Hello World!");
